fc82c228a4ccc09c6fdd3823b3ae0751950ef120
shiddieqy <shabriashiddieqy@gmail.com>
2023-07-22 11:38:35 +0700

    hub sprocket print

---
+++ AICTA/opencv/ball-tracking-master/ball_tracking.py
@@ -0,0 +1,52 @@
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the (optional) video file")
+args = vars(ap.parse_args())
+
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+
+if not args.get("video", False):
+    camera = cv2.VideoCapture(1)
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+while True:
+    (grabbed, frame) = camera.read()
+
+    if args.get("video") and not grabbed:
+        break
+
+    frame = imutils.resize(frame, width=600)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
+
---
+++ AICTA/opencv/object_tracking.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+cv2.namedWindow("Track")
+cv2.resizeWindow("Track",700,512)
+threshold_area = 1000
+
+def track(x):
+    return x
+
+cv2.createTrackbar('hue min', 'Track', 0,179,track)
+cv2.createTrackbar('hue max', 'Track', 179,179,track)
+cv2.createTrackbar('sat min', 'Track', 0,255,track)
+cv2.createTrackbar('sat max', 'Track', 255,255,track)
+cv2.createTrackbar('val min', 'Track', 0,255,track)
+cv2.createTrackbar('val max', 'Track', 255,255,track)
+cap = cv2.VideoCapture(0)
+tracker = cv2.legacy.TrackerMOSSE_create()
+# cap = cv2.VideoCapture("video.mp4")
+width  = cap.get(3)   # float `width`
+height = cap.get(4)
+id_no = []
+x_id = []
+y_id = []
+
+def is_inside(x,y,xg,yg,wg,hg):
+while (1):
+    
+    _,frame = cap.read() 
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos('hue min', 'Track')
+    h_max = cv2.getTrackbarPos('hue max', 'Track')
+    s_min = cv2.getTrackbarPos('sat min', 'Track')
+    s_max = cv2.getTrackbarPos('sat max', 'Track')
+    v_min = cv2.getTrackbarPos('val min', 'Track')
+    v_max = cv2.getTrackbarPos('val max', 'Track')
+
+
+    #mask
+    # lower = np.array([h_min, s_min, v_min])
+    # upper = np.array([h_max,s_max,v_max])
+    #mask green
+    lower = np.array([27, 86, 58])
+    upper = np.array([93,210,231])
+    mask = cv2.inRange(hsv_frame, lower,upper)
+
+
+    cn= cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    for i in cn:
+        # area = max(cn,key = cv2.contourArea)
+        area = cv2.contourArea(i)         
+        if area > threshold_area: 
+            (xg,yg,wg,hg)=cv2.boundingRect(i)
+            cv2.rectangle(frame,(xg,yg),(xg+wg,yg+hg),(0,255,0),3)
+            xcord = xg+wg/2-width/2
+            ycord = -1*(yg+hg/2-height/2)
+            print("x : ",xcord, "y : ",ycord)
+    cv2.drawMarker(frame, (int(width/2), int(height/2)),  (0, 0, 255), cv2.MARKER_CROSS, 10, 1);
+
+    cv2.imshow('ori',frame)
+    cv2.imshow('hsv',mask)
+    
+    if cv2.waitKey(1) %0xFF == ord('q'):
+        break
